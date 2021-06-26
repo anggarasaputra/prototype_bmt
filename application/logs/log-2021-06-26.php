9f351505caa8da7e15f8e56c460d64862faebd0d
angga.cahya <angga.cahya@gamatechno.com>
2021-06-26 22:38:56 +0700

    Input pertama

---
+++ application/logs/log-2021-06-26.php
@@ -0,0 +1,13 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-06-26 17:36:05 --> 404 Page Not Found: Login/index
+ERROR - 2021-06-26 18:04:45 --> Severity: Warning --> mkdir(): Invalid path /Users/anggasaputro/www/bank_angga/system/libraries/Session/drivers/Session_files_driver.php 136
+ERROR - 2021-06-26 18:04:45 --> Session: Configured save path '' is not a directory, doesn't exist or cannot be created.
+ERROR - 2021-06-26 18:04:45 --> Severity: Warning --> session_start(): Failed to initialize storage module: user (path: ) /Users/anggasaputro/www/bank_angga/system/libraries/Session/Session.php 143
+ERROR - 2021-06-26 18:10:55 --> 404 Page Not Found: Bank_angga/auth
+ERROR - 2021-06-26 22:08:15 --> Severity: Notice --> Undefined offset: 1 /Users/anggasaputro/www/bank_angga/application/libraries/MY_Encrypt.php 106
+ERROR - 2021-06-26 22:08:30 --> Severity: Notice --> Undefined offset: 1 /Users/anggasaputro/www/bank_angga/application/libraries/MY_Encrypt.php 106
+ERROR - 2021-06-26 22:18:16 --> Severity: Notice --> Undefined offset: 1 /Users/anggasaputro/www/bank_angga/application/libraries/MY_Encrypt.php 106
+ERROR - 2021-06-26 22:18:16 --> Severity: Notice --> Undefined offset: 1 /Users/anggasaputro/www/bank_angga/application/libraries/MY_Encrypt.php 106
+ERROR - 2021-06-26 22:19:26 --> Severity: Notice --> Undefined offset: 1 /Users/anggasaputro/www/bank_angga/application/libraries/MY_Encrypt.php 106
+ERROR - 2021-06-26 22:20:57 --> 404 Page Not Found: Test/index
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'faisalman/simple-excel-php' => array(
+            'pretty_version' => 'v0.3.15',
+            'version' => '0.3.15.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../faisalman/simple-excel-php',
+            'aliases' => array(),
+            'reference' => 'c392d9ee34cce210f034c9a9ef814f635320351c',
+            'dev_requirement' => false,
+        ),
+    ),
+);
